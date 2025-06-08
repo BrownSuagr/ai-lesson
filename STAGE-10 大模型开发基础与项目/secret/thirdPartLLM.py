89a5f13cfb9c0b707277fb2571f9bb1c8e8d7c3d
黑糖 <heitanghei@gmail.com>
2025-06-08 20:04:43 +0800

    LLM development unit course information

---
+++ STAGE-10 大模型开发基础与项目/secret/thirdPartLLM.py
@@ -0,0 +1,28 @@
+
+import requests
+import json
+
+def request_third_part_llm(model_name: str, content_json: json):
+    url = "https://www.dmxapi.cn/v1/chat/completions"
+
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': 'sk-D5PwNWjoJrBlulzHXstu37ps2AQbJkp8zYhgQmaHkvE58zOc', # 这里放你的 DMXapi key
+        'User-Agent': 'DMXAPI/1.0.0 (https://www.dmxapi.cn)',
+        'Content-Type': 'application/json'
+    }
+
+    payload = json.dumps({
+        # "model": "qwen2.5-72b-instruct",
+        # 这里是你需要访问的模型，改成上面你需要测试的模型名称就可以了。
+        "model": model_name,
+        "messages": content_json
+    })
+
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.json()
+
+if __name__ == '__main__':
+    msg = [{ "role": "user", "content": "周树人和鲁迅是兄弟吗？"}]
+    result = request_third_part_llm('qwen2.5-72b-instruct', msg)
+    print(result)
