fe3f9523dd29fe1c14fb336599eb0032086d2189
黑糖 <heitanghei@gmail.com>
2025-06-22 20:11:16 +0800

    other

--- STAGE-10 大模型开发基础与项目/secret/thirdPartLLM.py
+++ STAGE-10 大模型开发基础与项目/secret/thirdPartLLM.py
@@ -1,13 +1,19 @@
-
 import requests
 import json
 
+
 def request_third_part_llm(model_name: str, content_json: json):
+    '''
+    请求第三方模型集成平台
+    :param model_name:
+    :param content_json:
+    :return:
+    '''
     url = "https://www.dmxapi.cn/v1/chat/completions"
 
     headers = {
         'Accept': 'application/json',
-        'Authorization': 'sk-D5PwNWjoJrBlulzHXstu37ps2AQbJkp8zYhgQmaHkvE58zOc', # 这里放你的 DMXapi key
+        'Authorization': 'sk-D5PwNWjoJrBlulzHXstu37ps2AQbJkp8zYhgQmaHkvE58zOc',  # 这里放你的 DMXapi key
         'User-Agent': 'DMXAPI/1.0.0 (https://www.dmxapi.cn)',
         'Content-Type': 'application/json'
     }
@@ -22,7 +28,19 @@ def request_third_part_llm(model_name: str, content_json: json):
     response = requests.request("POST", url, headers=headers, data=payload)
     return response.json()
 
+from langchain_openai import ChatOpenAI
+def request_third_part_langChain(model_name: str, content_json: json):
+
+    model = ChatOpenAI(
+        model="gpt-4o-mini",
+        base_url="https://www.dmxapi.cn/v1",
+        api_key="sk-***************************************************",  # 替换成你的 DMXapi 令牌key
+    )
+    text = "周树人和鲁迅是兄弟吗？"
+    print(model(text))
+
+
 if __name__ == '__main__':
-    msg = [{ "role": "user", "content": "周树人和鲁迅是兄弟吗？"}]
+    msg = [{"role": "user", "content": "周树人和鲁迅是兄弟吗？"}]
     result = request_third_part_llm('qwen2.5-72b-instruct', msg)
     print(result)
